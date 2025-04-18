50816b7324910af98adcfd4657b9a9019acd9c46
moatassem47 <moatassem.320240182@ejust.edu.eg>
2025-04-18 13:07:36 +0000

    init file

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from .Baggage import Baggage
+from .CrewMember import CrewMember
+from .Feedback import Feedback
+from .Flight import Flight
+from .PassengerClass import PassengerClass
+from .ticket import Ticket
+
+__all__ = [
+    "Baggage",
+    "CrewMember",
+    "Feedback",
+    "Flight",
+    "PassengerClass",
+    "Ticket",
+]
